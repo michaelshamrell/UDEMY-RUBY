bcb072fdd54806ab0ee842aaa6f824909b6b8654
michaelshamrell <michaelshamrell2001@gmail.com>
2022-04-20 14:47:10 -0700

    Create notes on conversion between different classes in ruby.

--- Section2/Convert_Numbers_to_Strings_and_Vice_Versa.rb
+++ Section2/Convert_Numbers_to_Strings_and_Vice_Versa.rb
@@ -9,4 +9,33 @@ p str.class
 
 p str.to_i.class
 
-p 
+p str
+
+# to_f will convert an integer to a float class
+
+p str.to_f.class
+puts
+
+#Converting numbers to strings or floats
+
+number = 10
+
+p number.class
+p number.to_s
+
+p number.to_f
+p number.to_f.class
+puts
+
+#converting a floating number to an integer or string
+pi = 3.14
+p pi.to_i
+#converting floating numebrs to integers do not round up, they soully look at the integer and ignore fraction or decimal
+p pi.to_s
+p pi.to_s.class
+puts
+#These methods are available on objects even if they are that data type or class
+
+p "5".to_s.class
+p 10.to_i.class
+p 99.99.to_f.class
